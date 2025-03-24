2b67863c94a6332d45029410f8fb8dfcf15f263c
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-24 18:14:09 +0800

    studying arithmetic operator

--- index.js
+++ index.js
@@ -9,12 +9,28 @@
 //let forSale =true;
 //let isStudent = true;
 
+//let fullName = "Adrian";
+//let age = 25;
+//let isStudent = false;
 
-let fullName = "Adrian";
-let age = 25;
-let isStudent = false;
+//document.getElementById('p1').textContent = `Your name is ${fullName}`;
+//document.getElementById('p2').textContent = `You are ${age} years old`;
+//document.getElementById('p3').textContent = `Enrolled : ${isStudent}`;
 
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students % 2;
 
-document.getElementById('p1').textContent = `Your name is ${fullName}`;
-document.getElementById('p2').textContent = `You are ${age} years old`;
-document.getElementById('p3').textContent = `Enrolled : ${isStudent}`;
+//students += 1;
+//students -= 1;
+//students *=2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+let result = 12 % 5 + 8 / 2;
+
+console.log(result);
