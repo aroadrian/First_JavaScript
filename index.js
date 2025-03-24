15161ebdd6d6f97756da1d90220461edf3b7f370
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-24 19:21:28 +0800

    MATH in JS

--- index.js
+++ index.js
@@ -65,25 +65,14 @@
     //document.getElementById('myH3').textContent = `The circumference is ${circumference}` + 'cm';
 //};
 
-//Counter Program
+let x = 81;
+let y = 2;
+let z;
 
-const decreaseBtn = document.getElementById('decreaseBtn');
-const resetBtn  = document.getElementById('resetBtn');
-const increaseBtn = document.getElementById('increaseBtn');
-const countLabel = document.getElementById('countLabel');
-let count = 0;
-
-increaseBtn.onclick = function () {
-    count++;
-    countLabel.textContent = count;
-};
-
-decreaseBtn.onclick = function () {
-    count--;
-    countLabel.textContent = count;
-};
-
-resetBtn.onclick = function () {
-    count = 0;
-    countLabel.textContent = count;
-};
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+z = Math.sqrt(x);
+console.log(z);
