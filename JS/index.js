7e78652fc9d0ed86a685b3d1a9ad918df69f3961
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-24 19:43:23 +0800

    random generator number

--- JS/index.js
+++ JS/index.js
@@ -65,14 +65,14 @@
     //document.getElementById('myH3').textContent = `The circumference is ${circumference}` + 'cm';
 //};
 
-let x = 81;
-let y = 2;
-let z;
+//let x = 81;
+//let y = 2;
+//let z;
 
 //z = Math.round(x);
 //z = Math.floor(x);
 //z = Math.ceil(x);
 //z = Math.trunc(x);
 //z = Math.pow(x, y);
-z = Math.sqrt(x);
-console.log(z);
+//z = Math.sqrt(x);
+//console.log(z);
