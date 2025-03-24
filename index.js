8f6f18633cda505b0c36e18cc931c803d0e9430b
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-24 18:02:20 +0800

    variable and displaying the content using this id

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-document.getElementById("myH1").textContent = "Hello World!";
-document.getElementById("myP").textContent = "How are you today?";
+//document.getElementById("myH1").textContent = "Hello World!";
+//document.getElementById("myP").textContent = "How are you today?";
+
+//let age =25;
+
+//let firstName = "Adrian";
+
+//let online = true;
+//let forSale =true;
+//let isStudent = true;
+
+
+let fullName = "Adrian";
+let age = 25;
+let isStudent = false;
+
+
+document.getElementById('p1').textContent = `Your name is ${fullName}`;
+document.getElementById('p2').textContent = `You are ${age} years old`;
+document.getElementById('p3').textContent = `Enrolled : ${isStudent}`;
