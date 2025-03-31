f2bede69540d9311c97eed4b9776bac88710438c
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-31 17:54:15 +0800

    Spread operator in JS

--- JS/index.js
+++ JS/index.js
@@ -369,3 +369,23 @@
   //const rowString = row.join(' ');
   //console.log(rowString);
 //}
+
+// Spread Operator
+
+//let numbers = [1, 2, 3, 4, 5];
+//let maximum = Math.max(...numbers);
+//let minimum = Math.min(...numbers);
+
+//console.log(maximum);
+//console.log(minimum);
+
+//let username = "adrian aro";
+//let letters = [...username].join("-");
+
+//console.log(letters);
+
+let fruits = ["apple", "banana", "orange"];
+let vegetables = ["carrots", "celery", "potatoes"];
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
