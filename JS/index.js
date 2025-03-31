965989884cdc3b58f7ae935e1c91f81fa672239a
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-31 19:30:55 +0800

    foreach() method in Js

--- JS/index.js
+++ JS/index.js
@@ -457,17 +457,59 @@
 //function goodbye(){
   //console.log("Goodbye!");
 //}
-sum(displayPage, 1, 2);
+//sum(displayPage, 1, 2);
 
-function sum(callback, x, y){
-    let result = x+y;
-    callback(result);
+//function sum(callback, x, y){
+  //  let result = x+y;
+ //   callback(result);
+//}
+
+//function displayConsole(result){
+ //   console.log(result);
+//}
+
+//function displayPage(result){
+ //   document.getElementById("myH1").textContent = result;
+//}
+
+//forEach()
+
+//let numbers = [1, 2, 3, 4, 5];
+//numbers.forEach(cube);
+//numbers.forEach(display);
+
+//function double(element, index, array){
+ // array[index] = element * 2;
+//}
+
+//function triple(element, index, array){
+//  array[index] = element * 3;
+//}
+
+//function square(element, index, array){
+ /// array[index] = Math.pow(element, 2);
+//}
+//function cube(element, index, array){
+  //array[index] = Math.pow(element, 3);
+//}
+
+//function display(element){
+//    console.log(element);
+//}
+
+let fruits = ["banna", "orange", "apple", "coconut"];
+
+fruits.forEach(capitalize);
+fruits.forEach(display);
+
+function upperCase(element, index, array){
+  array[index] = element.toUpperCase();
 }
 
-function displayConsole(result){
-    console.log(result);
+function capitalize(element, index, array){
+  array[index] = element.charAt(0).toUpperCase() + element.slice(1);
 }
 
-function displayPage(result){
-    document.getElementById("myH1").textContent = result;
+function display(element){
+  console.log(element);
 }
