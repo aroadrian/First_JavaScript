7449b9ceecfc270478d99e5852433d68610f5f9e
Adrian Aro <aroadrianchristopher@gmail.com>
2025-04-03 23:28:30 +0800

    object in JS

--- JS/index.js
+++ JS/index.js
@@ -591,7 +591,41 @@
 //setTimeout(() => console.log("hello"), 3000);
 
 
-const numbers = [1, 2, 3, 4, 5, 6];
+//const numbers = [1, 2, 3, 4, 5, 6];
 
-const squares = numbers.map((element) => Math.pow(element, 2));
-console.log(squares);
+//const squares = numbers.map((element) => Math.pow(element, 2));
+//console.log(squares);
+
+// Object
+
+const person1 = {
+                firstName: "Adrian",
+                lastName: "Aro",
+                age: 26,
+                isEmployed: true,   
+                sayHello: function(){console.log("Hi Hello")},
+                eat: () => console.log("pizza"),
+
+                  };
+
+const person2 = {firstName: "Reslychen",
+                  lastName: "Aro",
+                  age: 26,
+                  isEmployed: false,  
+                  sayHello: () => console.log("Edi wow "),
+                  eat: () => console.log("pizza"),
+                };
+
+person1.sayHello();
+person1.eat();
+person2.sayHello();
+person2.eat();
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isEmployed);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
