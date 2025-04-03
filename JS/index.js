deeb6db9321ad1af5a02c1efdaaea60129303211
Adrian Aro <aroadrianchristopher@gmail.com>
2025-04-03 23:09:34 +0800

    function expression in js

--- JS/index.js
+++ JS/index.js
@@ -572,4 +572,12 @@ const squares = numbers.map(function(element){
   return Math.pow(element, 2);
 });
 
-console.log(squares);
+const evenNum = numbers.filter(function(element){
+    return element % 2 === 0;
+})
+
+const total = numbers.reduce(function(accumulator, element){
+    return accumulator + element;
+})
+
+console.log(total);
